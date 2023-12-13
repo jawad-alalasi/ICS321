9b2ccbef5cc0632542f390c4f082ba5a4a28b21c
jawad-alalasi <jawad-alalasi@gmail.com>
2023-12-13 17:06:39 +0300

    new

--- backend/app.js
+++ backend/app.js
@@ -133,6 +133,60 @@ app.post('/bloodcount', function(req, res, next) {
     });
   /*res.redirect()*/
 });
+app.post('/COLLOCTION', function(req, res, next) {
+    var sqlcoun = "SELECT blood_drive.NUMBER, COUNT(blood_drive_donation.SAMPLE_NUMBER) AS expr1 FROM blood_drive_donation INNER JOIN blood_drive ON blood_drive_donation.NUMBER = blood_drive.NUMBERGROUP BY blood_drive.NUMBER";
+    db.query(sqlcoun, function(err, result) {
+        if (err) throw err;
+        res.render('COLLOCTION', result) 
+        console.log('record inserted');
+    });
+  /*res.redirect()*/
+});
+app.post('/Paymentcon', function(req, res, next) {
+    var sqlcoun = "SELECT receive.ID, receive.SAMPLE_NUMBER, person.NAME FROM receive INNER JOIN recipient ON receive.ID = recipient.ID INNER JOIN person ON recipient.ID = person.ID WHERE receive.PAYMENT = 1";
+    db.query(sqlcoun, function(err, result) {
+        if (err) throw err;
+        res.render('Paymentcon', result) 
+        console.log('record inserted');
+    });
+  /*res.redirect()*/
+});
+app.post('/report1', function(req, res, next) {
+    var date=req.body.date;
+    if (date) {
+        var sqlcoun = "SELECT donate.ID, person.NAME FROM donate INNER JOIN donor ON donate.ID = donor.ID INNER JOIN person ON donor.ID = person.ID ";
+        db.query(sqlcoun, function(err, result) {
+            if (err) throw err;
+            res.render('repor1', result) 
+            console.log('record inserted');
+        });
+    }else{
+    var sqlcoun = "SELECT donate.ID, person.NAME FROM donate INNER JOIN donor ON donate.ID = donor.ID INNER JOIN person ON donor.ID = person.ID WHERE ID < 8";
+    db.query(sqlcoun, function(err, result) {
+        if (err) throw err;
+        res.render('repor1', result) 
+        console.log('record inserted');
+    });}
+  /*res.redirect()*/
+});
+app.post('/report0', function(req, res, next) {
+    var date=req.body.date;
+    if (date) {
+        var sqlcoun = "SELECT person.NAME, receive.SAMPLE_NUMBER, person.ID FROM recipient INNER JOIN person ON recipient.ID = person.ID INNER JOIN receive ON receive.ID = recipient.ID ";
+        db.query(sqlcoun, function(err, result) {
+            if (err) throw err;
+            res.render('repor0', result) 
+            console.log('record inserted');
+        });
+    }else{
+    var sqlcoun = "SELECT person.NAME, receive.SAMPLE_NUMBER, person.ID FROM recipient INNER JOIN person ON recipient.ID = person.ID INNER JOIN receive ON receive.ID = recipient.ID WHERE ID < 8";
+    db.query(sqlcoun, function(err, result) {
+        if (err) throw err;
+        res.render('repor0', result) 
+        console.log('record inserted');
+    });}
+  /*res.redirect()*/
+});
 
 // port must be set to 3000 because incoming http requests are routed from port 80 to port 8080
 app.listen(3000, function () {
